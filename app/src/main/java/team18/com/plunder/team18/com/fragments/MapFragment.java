91f8cd7b59383a2245ce801e4f7db5ce808f75f1
Work Account <szy.jackiewicz@gmail.com>
2017-02-08 18:58:00 +0000

    Added Map to the Map button and the map Fragment class now gets called

--- app/src/main/java/team18/com/plunder/team18/com/fragments/MapFragment.java
+++ app/src/main/java/team18/com/plunder/team18/com/fragments/MapFragment.java
@@ -1,11 +1,19 @@
 package team18.com.plunder.team18.com.fragments;
 
-import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import team18.com.plunder.R;
 
@@ -13,13 +21,74 @@ import team18.com.plunder.R;
  * Created by Szymon Jackiewicz on 2/6/2017.
  */
 
-public class MapFragment extends Fragment {
+public class MapFragment  extends Fragment {
 
-    @Nullable
+    MapView mMapView;
+    private GoogleMap googleMap;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main_map, container, false);
+
+        mMapView = (MapView) rootView.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume(); // needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                googleMap = mMap;
+
+                // For showing a move to my location button
+                //googleMap.setMyLocationEnabled(true);
+
+                // Map functionality
+                mMap.setMapType(mMap.MAP_TYPE_HYBRID);
+                mMap.setMinZoomPreference(15f); //20 looks good
+                mMap.setBuildingsEnabled(true);
+
+                // For dropping a marker at a point on the Map
+                LatLng newcastleUniversityCoordinates = new LatLng(54.979177, -1.614806);
+                mMap.addMarker(new MarkerOptions().position(newcastleUniversityCoordinates).title("Hello World"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(newcastleUniversityCoordinates));
+
+                // For zooming automatically to the location of the marker
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(newcastleUniversityCoordinates).zoom(12).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        });
+
         return rootView;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
 }
